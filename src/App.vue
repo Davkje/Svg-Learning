469dcfd061332adec6fe08e3ac78a46231161702
David Kjellstrand <david.kjellstrand@gmail.com>
2025-03-12 15:10:45 +0100

    feat: installed vue and copied previous project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeComp from "./components/HomeComp.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<img class="bgimg" src="/src/assets/map1.webp" alt="Background image of a map">
+	<div class="filter"></div>
+	<div class="wrapper">
+		<HomeComp />
+	</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style lang="scss" scoped>
+.filter {
+	position: absolute;
+	background-color: #070723f1;
+	width: 100vw;
+	height: 100vh;
+	mix-blend-mode: multiply;
+	z-index: 1;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.wrapper {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+  z-index: 10;
 }
 </style>
